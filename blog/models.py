004de502b6f454e541899e616a619383d712c87d
Mawadda Kadi <mawadda.kadi@gmail.com>
2024-02-25 15:48:20 +0000

    add fixtures to populate posts

--- blog/models.py
+++ blog/models.py
@@ -7,26 +7,26 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    auther = models.ForeignKey(
+    author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excrept = models.TextField(blank=True)
+    excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created_on"]
-        
+
     def __str__(self):
-        return f"{self.title} | written by {self.auther}"
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments"
     )
-    auther = models.ForeignKey(
+    author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter"
     )
     body = models.TextField()
@@ -37,4 +37,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.auther}"
+        return f"Comment {self.body} by {self.author}"
